b58032cd959836f277d634f7a98bfe35d1dbd596
Dewayne Allen <dewayneallen76@gmail.com>
2017-03-27 17:11:51 -0500

    changed the variable name numberOfCoffees to quantity

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -27,9 +27,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int numberOfCoffees = 2;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5);
+        int quantity = 2;
+        display(quantity);
+        displayPrice(quantity * 5);
 
     }
 
